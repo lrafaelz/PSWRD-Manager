b1fc472b7755857881691ebd16672095e87b97cc
GuilhermeCaneda <56172397+GuilhermeCaneda@users.noreply.github.com>
2023-06-27 14:15:50 -0300

    Add files via upload
    added password generator module

---
+++ password-generator/requirement.py
@@ -0,0 +1,38 @@
+class Requirement():
+
+  def __init__(self):
+    self.uppercase = 0
+    self.lowercase = 0
+    self.special = 0
+    self.number = 0
+
+  def charactersAmount(self):
+    return self.uppercase + self.lowercase + self.special + self.number
+
+  ### GETTERS AND SETTERS ###
+
+  def setUppercase(self, uppercase):
+    self.uppercase = uppercase
+
+  def setLowercase(self, lowercase):
+    self.lowercase = lowercase
+
+  def setSpecial(self, special):
+    self.special = special
+
+  def setNumber(self, number):
+    self.number = number
+
+  def getUppercase(self):
+    return self.uppercase
+
+  def getLowercase(self):
+    return self.lowercase
+
+  def getSpecial(self):
+    return self.special
+
+  def getNumber(self):
+    return self.number
+
+  ###########################                     
